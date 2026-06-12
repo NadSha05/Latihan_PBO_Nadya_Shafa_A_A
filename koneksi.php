3a9589c577713a50235a8450e4b07c25b834ed3e
Your Namegit config --global user.name galangsusanto76-beep <galangsusanto@gmail.com>
2026-06-12 11:48:12 +0700

    menambahkan file tahap 3

---
+++ koneksi.php
@@ -0,0 +1,17 @@
+<?php
+// Konfigurasi Database
+$host = "localhost";
+$user = "root";       // Username default XAMPP/MariaDB
+$pass = "";           // Password default XAMPP/MariaDB biasanya kosong
+$db   = "db_latihan_pbo_trpl1a_nadya_shafa_a_a";
+
+// Membuat Koneksi
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Memeriksa Koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+} else {
+    echo "Koneksi berhasil ke database!";
+}
+?>
