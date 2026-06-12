2eebd184fbc7cd6b328adc5d84d30ab08f85fe73
Your Namegit config --global user.name galangsusanto76-beep <galangsusanto@gmail.com>
2026-06-12 11:54:18 +0700

    menambahkan file tahap 4 & 5(dengan rumus tahap 5)

---
+++ tiketVelvet.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'Tiket.php';
+
+class TiketVelvet extends Tiket {
+    private $bantalSelimutPack;
+    private $layananButler;
+
+    public function __construct($id, $film, $jadwal, $kursi, $harga, $pack, $butler) {
+        parent::__construct($id, $film, $jadwal, $kursi, $harga);
+        $this->bantalSelimutPack = $pack;
+        $this->layananButler = $butler;
+    }
+
+    public function hitungTotalHarga() {
+        // Total Harga = (jumlah_kursi * hargaDasarTiket) * 1.50
+        return ($this->jumlah_kursi * $this->harga_dasar_tiket) * 1.50;
+    }
+
+    public function tampilkanInfoFasilitas() {
+        return "Paket: " . ($this->bantalSelimutPack ? "Ya" : "Tidak") . ", Butler: " . ($this->layananButler ? "Ya" : "Tidak");
+    }
+}
